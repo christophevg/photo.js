3120204a491eeeb244985ff3be5dd5563522f06d
Christophe VG <xtof@pumpkin.local>
2011-08-24 14:12:01 +0200

    changed id's from entry-urls to simply the id

--- src/providers/google.js
+++ src/providers/google.js
@@ -9,6 +9,27 @@
     document.getElementsByTagName("head")[0].appendChild(e); 
   },
   
+  extractAlbumId = function extractAlbumId( url ) {
+    if( matches = url.match( /\/albumid\/([0-9]+)/ ) ) {
+      return matches[1];
+    }
+    return null;
+  },
+  
+  extractPhotoId = function extractPhotoId( url ) {
+    if( matches = url.match( /\/photoid\/([0-9]+)/ ) ) {
+      return matches[1];
+    }
+    return null;
+  },
+  
+  constructEntryUrl = function constructEntryUrl( user, albumId, photoId ) {
+    return "https://picasaweb.google.com/data/entry/base/"
+          + "user/"  + user
+          + ( albumId ? "/albumid/" + albumId : "" )
+          + ( photoId ? "/photoid/" + photoId : "" );
+  },
+  
   // executed in scope of viewer
   processAlbums = function processAlbums( data ) {
     var albums = data.feed.entry;
@@ -19,7 +40,7 @@
       var id = album.link[0].href.replace( /\?.*$/, "" );
 
       this.addAlbum( {
-        id     : id,
+        id     : extractAlbumId(id),
         title  : album.title.$t,
         thumb  : album['media$group']['media$thumbnail'][0].url
       } );
@@ -27,18 +48,20 @@
       var cb = registerCallback( (function(viewer) { 
         return function(data) { processPhotos.call(viewer, data); } })(this));
       var base = album.link[0].href;
-      var url = base + "&kind=photo&thumbsize=75,100&callback=__google_photo_cb__["+cb+"]";
+      var url = base + "&kind=photo&max-results=1000&thumbsize=75,100" 
+                     + "&hl=en_US&access=public" +
+                     "&callback=__google_photo_cb__[" + cb + "]";
       get( url );
     }
   },
     
   // executed in scope of viewer
   processPhotos = function processPhotos( data ) {
-    var albumId  = data.feed.id.$t;
+    var albumId  = extractAlbumId(data.feed.id.$t);
     var photos = data.feed.entry;
     for( var p=0; p<photos.length; p++ ) {
       var photo = photos[p];
-      var id = photo.id.$t.replace( /\?.*$/, "" );
+      var id = extractPhotoId(photo.id.$t.replace( /\?.*$/, "" ));
       this.addPhoto( albumId, {
         id      : id,
         title   : photo.title.$t,
@@ -65,15 +88,17 @@
   
   provider.connection = function connection( userId ) {
     this.userId = userId;
+    this.handleOnLoad = null;
   };
 
   provider.connection.prototype.populate = function populate( viewer ) {
     var cb = registerCallback( function(data) { 
       processAlbums.call(viewer, data);
     } );
+    
     var base = "https://picasaweb.google.com/data/feed/base/user/" 
-               + this.userId + "?alt=json-in-script";
-    var url = base + "&kind=album&callback=__google_photo_cb__["+cb+"]";
+               + this.userId + "?alt=json-in-script&hl=en_US&access=public";
+    var url = base + "&kind=album&max-results=1000&callback=__google_photo_cb__["+cb+"]";
     get( url );
   };
   
